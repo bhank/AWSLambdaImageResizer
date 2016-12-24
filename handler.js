ab727b85bec76ddb34a7ccd9a6a8cf351081ae87
Adam Coyne <github@mail2.coyne.nu>
2016-12-24 13:36:14 -0600

    Clean up and get things working

--- handler.js
+++ handler.js
@@ -14,59 +14,78 @@ module.exports.handler = (event, context, callback) => {
         const regex = /^(?:\/w(\d+))?\/(.*\.(.+?))$/;
         const [,resizeWidth,path,extension] = originalPath.match(regex);
         const contentType = getContentType(extension);
-        const key = root + path;
+        const key = decodeURI(root + path); // TODO: test with weird chars to see if any encoding/decoding is necessary. Lambda rejects URLs with [] brackets, so I'll need to encode those, I guess, and decode here. Ugly.
 
         getFileFromS3AndContinue(bucket, region, key, resizeWidth, contentType, callback);
     } catch (errorMessage) {
+        returnErrorResponse(errorMessage, callback);
+    }
+};	
+
+function returnErrorResponse(errorMessage, callback) {
         console.log(errorMessage);
 
         const response = {
             statusCode: 400,
-            body: errorMessage
+            body: 'Error: ' + JSON.stringify(errorMessage)
         };
 
         callback(null, response);
-    }
-};	
+}
 
 function returnResponse(content, contentType, callback) {
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Content-Type": contentType
-        },
-        body: content.toString("base64"),
-        isBase64Encoded: true
-    };
-    callback(null, response);
+    try {
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Content-Type": contentType
+            },
+            body: content.toString("base64"),
+            isBase64Encoded: true
+        };
+        callback(null, response);
+    } catch (errorMessage) {
+        returnErrorResponse(errorMessage, callback);
+    }
+    
 }
 
 function resizeAndContinue(content, resizeWidth, contentType, callback) {
-    console.log('resizeAndContinue:', arguments);
-    // TODO: resize...
-    returnResponse(content, contentType, callback);
+    try {
+        console.log('resizeAndContinue:', arguments);
+        // TODO: resize...
+        returnResponse(content, contentType, callback);
+    } catch (errorMessage) {
+        returnErrorResponse(errorMessage, callback);
+    }
+    
 }
 
 function getFileFromS3AndContinue(Bucket, Region, Key, resizeWidth, contentType, callback) {
-    console.log('getFileFromS3AndContinue:', arguments);
-    const opts = {};
+    try {
+        console.log('getFileFromS3AndContinue:', arguments);
+        const opts = {};
 
-    // Optionally provide credentials in the Lambda environment config, in case the Lambda account doesn't have access to the S3 bucket.
-    opts.accessKeyId = process.env.S3KEY;
-    opts.secretAccessKey = process.env.S3SECRET;
+        // Optionally provide credentials in the Lambda environment config, in case the Lambda account doesn't have access to the S3 bucket.
+        opts.accessKeyId = process.env.S3KEY;
+        opts.secretAccessKey = process.env.S3SECRET;
 
-    if(Region) {
-        opts.region = Region;
-    }
-
-    var s3 = new AWS.S3(opts);
-    s3.getObject({Bucket, Key}, (err, result) => {
-        console.log('getObject:', err, result);
-        if(err) {
-            throw new Error(err);
+        if(Region) {
+            opts.region = Region;
         }
-        resizeAndContinue(result.Body, resizeWidth, contentType, callback);
-    })
+
+        var s3 = new AWS.S3(opts);
+        s3.getObject({Bucket, Key}, (err, result) => {
+            console.log('getObject:', err, result);
+            if(err) {
+                returnErrorResponse(err, callback);
+            } else {
+                resizeAndContinue(result.Body, resizeWidth, contentType, callback);
+            }
+        });
+    } catch (errorMessage) {
+        returnErrorResponse(errorMessage, callback);
+    }
 }
 
 function getContentType(extension) {
