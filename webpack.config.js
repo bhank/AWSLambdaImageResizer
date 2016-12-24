662260344e09be0a48c4eed18d1922d934fba540
Adam Coyne <github@mail2.coyne.nu>
2016-12-24 10:05:01 -0600

    Transpile with babel

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target: 'node', // don't bundle native node modules // http://stackoverflow.com/questions/33001237/webpack-not-excluding-node-modules
+    externals: [nodeExternals()], // don't bundle external node modules
+    entry: './handler.js',
+    output: {
+        libraryTarget: 'commonjs2',
+        filename: "./dist/main.js"
+    },    
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }    
+}
